97ef9f5e419f73237052dd21631960bf666d302c
MichaelBecker28 <michael.s.becker02@outlook.com>
2025-03-05 16:21:43 -0600

    changes to shell.c

--- Courses/cpre_308/projects/project_01_unix_shell/shell.c
+++ Courses/cpre_308/projects/project_01_unix_shell/shell.c
@@ -1,2 +1,96 @@
-#include <stdio.h>
-#include <stdlib.h>
+
+//Include library statements for shell
+
+#include <stdio.h>      // I/O functions (printf, etc.)
+#include <stdlib.h>     // standard lib
+#include <string.h> 
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>      // For error codes
+#include <sys/wait.h>   // For process waiting
+
+//Constants and define statements
+
+#define max_in 1024 //sets the max input line length
+#define max_arg 64  // max number of arguments
+#define prompt "308sh>" //This would be the defaut shell prompt
+
+
+// function prototypes to define functions
+
+void input_parse (char *input,char **arguments, int *background); // will split input into separate arguments and will check if the command should run in the background
+int  built_in (char **arguments);  //for built in shell commands
+void execute_program(char **arguments,int background);// (arguments)-array containing command. (background)- flag to run in background
+void background_proc_check();//check if any background processes have completed.
+
+//Main Function
+
+int main (int arg_count, char **arg_array){
+    //variables
+    char *shell_prompt = prompt;
+    char input[max_in];
+    char *args[max_arg];
+    int background;
+
+    /*
+    This for loop gets the command line argument and checks if it 
+    is a special prompt. It checks if the argument is "-p" 
+    */
+    for (int i=1; i<arg_count;i++){
+        if(strcmp(arg_array[i],"-p")== 0&& i+1 <arg_count){
+           shell_prompt = arg_array[i+1];
+           i++;
+        }
+    }
+
+    // main loop for the shell
+
+    while (1){
+        background_proc_check();
+
+        //display a prompt:
+        printf("%s ",shell_prompt);
+        //force display
+        fflush(stdout);
+
+        if (fgets(input, sizeof(input), stdin)==NULL){
+        break;
+        }
+    
+
+    //parse the input
+    input_parse(input, args,&background);
+
+    //Continue if there was no command
+    if (args[0]== NULL){
+        continue;
+    }
+
+    //try different commands built in and not
+    if (!built_in(args)){
+        execute_program(args, background);
+
+    }
+    }
+
+    return 0;
+}
+
+/**
+ * This section will contain my helper methods that were seen at the top
+ * 
+ * **********************************************************************
+ * 
+ */
+
+ /**
+  * Parse the input into separate sections and arguments
+  * 
+  * @param input the user input being parsed
+  * @param arguments array that stores the parsed inputs
+  * @param background background pointer for flagging 
+  */
+ void input_parse(char *input, char **arguments, int *background){
+    int i =0;
+    
+ }
